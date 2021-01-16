40aad93d0c455df441df556c014e8c2ec8e5ec74
truesspeak <kola-kola-321>
2021-01-16 18:48:39 +0300

    Added a few tests for notes_controller.rb

--- spec/controllers/notes_controller_spec.rb
+++ spec/controllers/notes_controller_spec.rb
@@ -118,4 +118,143 @@ RSpec.describe NotesController, type: :controller do
       end
     end
   end
+
+  describe "POST #create" do
+    context 'guest' do
+      it 'tries to create a new note' do
+        expect do
+          post :create, params: { note: { user_id: another_user.id, title: 'new_title', description: 'new desc' } }
+        end.to_not change(Note, :count)
+      end
+    end
+
+    context 'user' do
+      before { login(user) }
+
+      it 'tries to create a new note with valid attributes' do
+        expect do
+          post :create, params: { note: { user_id: user.id, title: 'new_title', description: 'new desc' } }
+        end.to change(Note, :count).by(1)
+      end
+
+      it 'redirect after create note' do
+        post :create, params: { note: { user_id: user.id, title: 'new_title', description: 'new desc' } }
+        expect(response).to redirect_to note_path(Note.last.id)
+      end
+
+      it 'tries to create a new note with invalid attributes' do
+        expect do
+          post :create, params: { note: { user_id: user.id } }
+        end.to_not change(Note, :count)
+      end
+
+      it 'render new form when note params invalid' do
+        post :create, params: { note: { user_id: user.id } }
+        expect(response).to render_template(:new)
+      end
+    end
+  end
+
+  describe "GET #new" do
+    context 'guest' do
+      it 'redirect after create note' do
+        get :new
+        expect(response).to redirect_to root_path
+      end
+    end
+
+    context 'authenticated user' do
+      before { login(user) }
+      it 'redirect to new form' do
+        get :new
+        expect(response).to render_template(:new)
+      end
+    end
+  end
+
+  describe "PATCH #update" do
+    let!(:note) { create(:note, user: user) }
+
+    context 'guest' do
+      it 'tries to update note' do
+        patch :update, params: { id: note.id, note: { title: 'new_title', description: 'new desc' } }
+        note.reload
+
+        expect(note.title).to_not eq 'new_title'
+        expect(note.description).to_not eq 'new desc'
+        expect(response).to redirect_to root_path
+      end
+    end
+
+    context 'owner' do
+      before { login(user) }
+
+      it 'tries to update note with valid data' do
+        patch :update, params: { id: note.id, note: { title: 'new_title', description: 'new desc' } }
+        note.reload
+
+        expect(note.title).to eq 'new_title'
+        expect(note.description).to eq 'new desc'
+        expect(response).to redirect_to note_path(note.id)
+      end
+
+      it 'tries to update note with valid data' do
+        patch :update, params: { id: note.id, note: { title: nil, description: nil } }
+        note.reload
+
+        expect(response).to render_template :edit
+      end
+    end
+
+    context 'not owner' do
+      before { login(another_user) }
+
+      it 'tries to update note' do
+        patch :update, params: { id: note.id, note: { title: 'new_title', description: 'new desc' } }
+
+        note.reload
+
+        expect(note.title).to_not eq 'new_title'
+        expect(note.description).to_not eq 'new desc'
+        expect(response).to redirect_to notes_path
+      end
+    end
+  end
+
+  describe "DELETE #destroy" do
+    let!(:note) { create(:note, user: user) }
+
+    context 'guest' do
+      it 'try to destroy note' do
+        expect do
+          delete :destroy, params: { id: note.id }
+        end.to_not change(Note, :count)
+
+        expect(response).to redirect_to root_path
+      end
+    end
+
+    context 'owner' do
+      before { login(user) }
+      it 'try to destroy note' do
+        expect do
+          delete :destroy, params: { id: note.id }
+        end.to change(Note, :count).by(-1)
+
+        expect(response).to redirect_to notes_path
+      end
+    end
+
+    context 'owner' do
+      before { login(another_user) }
+
+      it 'try to destroy note' do
+        expect do
+          delete :destroy, params: { id: note.id }
+        end.to_not change(Note, :count)
+
+        expect(response).to redirect_to notes_path
+      end
+    end
+  end
 end
