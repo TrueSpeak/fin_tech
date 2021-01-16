91efafe01b97b9464638d1cf319d28b86ea58ee8
truesspeak <kola-kola-321>
2021-01-16 13:10:13 +0300

    config for rails_helper.rb

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -32,6 +32,14 @@ rescue ActiveRecord::PendingMigrationError => e
   exit 1
 end
 RSpec.configure do |config|
+  config.include FactoryBot::Syntax::Methods
+
+  config.before(:suite) do
+    DatabaseCleaner.clean_with(:truncation)
+    DatabaseCleaner.strategy = :transaction
+  end
+
+  config.include Devise::Test::ControllerHelpers, type: :controller
   # Remove this line if you're not using ActiveRecord or ActiveRecord fixtures
   config.fixture_path = "#{::Rails.root}/spec/fixtures"
 
